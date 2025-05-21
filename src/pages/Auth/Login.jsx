12056eab576d0592abaa5a805aa3fcb34b358f53
Ghruank <ghruankkothare@gmail.com>
2025-05-21 08:25:43 +0530

    made changes to tailwind

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -5,10 +5,10 @@ const Login = () => {
   return (
     <AuthLayout>
     <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
-      <h3 className="text-xl font-semibold text-black">
+      <h3 className="text-xl font-semibold text-green-600">
         Welcome back !!
       </h3>
-      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+      <p className="text-xs text-green-700 mt-[5px] mb-6">
         Please Enter Your Details To Log In
       </p>
     </div>
